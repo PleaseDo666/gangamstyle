392626eacc8aa066465e41341bcfc099f6b4341a
sarahzdev <sarah.hg.zhang@gmail.com>
2023-04-11 15:03:57 -0700

    wip: comment out methods for now

--- sidebars.js
+++ sidebars.js
@@ -161,7 +161,7 @@ const sidebars = {
               items: [
                 'SDK/Explanation/authentication/authSig',
                 'SDK/Explanation/authentication/sessionSigs',
-                'SDK/Explanation/authentication/methods',
+                // 'SDK/Explanation/authentication/methods',
               ],
             },
             'SDK/Explanation/tests'
